dd6f54290bbbeeb512a76e5f4a42541945088e00
yangyang <mengyangyang@speechocean.com>
2023-04-19 12:05:23 +0800

    Remove api key

--- notebook/test.py
+++ notebook/test.py
@@ -1,7 +1,7 @@
 # encoding: utf8
 
 import openai
-openai.api_key = "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"
+openai.api_key = ""
 
 inputs = ["How to train LLM?"]
 print(openai.Embedding.create(input=inputs, model="text-embedding-ada-002"))
